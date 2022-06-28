cb3414e099e72436343f95b9a3e14f7efcd0ced6
Rodrigo Rios <rodrigoalejandrorios94@gmail.com>
2022-06-28 17:44:09 -0300

    Clase 2: Theme customization

---
+++ src/config/theme.config.tsx
@@ -0,0 +1,47 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import React from "react";
+
+type ThemeProp = {
+  children: JSX.Element;
+};
+
+export enum themePalette {
+  BG = "#12181b",
+  LIME = "#C8FA5F",
+  FONT_GLOBAL = "'JetBrains Mono', monospace",
+}
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: themePalette.BG,
+    },
+    primary: {
+      main: themePalette.LIME,
+    },
+  },
+  typography: {
+    fontFamily: themePalette.FONT_GLOBAL,
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        style: {
+          textTransform: "none",
+          boxShadow: "none",
+          borderRadius: "0.5em",
+        },
+      },
+    },
+  },
+});
+
+export const ThemeConfig: React.FC<ThemeProp> = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeConfig } from './config/theme.config';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ThemeConfig>
+
     <App />
+    </ThemeConfig>
   </React.StrictMode>
 );
 
