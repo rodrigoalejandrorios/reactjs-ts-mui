555678ea2ac3494b0b8c9e5d9e9dfa704ce5c624
Rodrigo Rios <rodrigo.rios@naranjax.com>
2022-08-22 17:34:18 -0300

    Clase 7: APIs

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Button, Container } from "@mui/material";
 import { HeaderComponent } from "../../components";
+import { characters } from "../../api/characters";
 
 export const HomePage: React.FC<{}> = () => {
+
+
+  React.useEffect(()=>{
+    characters.getById({id: 1}).then((r)=>{
+      console.log(r.data)
+    }).catch((e)=>{
+      console.error(e)
+    })
+  },[])
+
   return (
     <Container maxWidth="xl">
       <HeaderComponent
