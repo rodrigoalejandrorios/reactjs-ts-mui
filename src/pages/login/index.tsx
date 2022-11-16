8aed240c0f52d2f5e25c686765a96270f2bc9cda
Codrr <coderrdev@gmail.com>
2022-11-16 17:07:26 -0300

    Form validation with formik

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Box,
   Button,
@@ -7,9 +7,10 @@ import {
   Paper,
   TextField,
   Typography,
-} from "@mui/material";
-import { useNotification } from "../../context/notification.context";
-import { LoginValidate } from "../../utils/validateForm";
+} from '@mui/material';
+import { useNotification } from '../../context/notification.context';
+import { LoginValidate } from '../../utils/validateForm';
+import { useFormik } from 'formik';
 
 type LoginType = {
   username: string;
@@ -17,26 +18,17 @@ type LoginType = {
 };
 
 export const LoginPage: React.FC<{}> = () => {
-  const { getError, getSuccess } = useNotification();
-  const [loginData, setLoginData] = React.useState<LoginType>({
-    username: "",
-    password: "",
+  const { getSuccess } = useNotification();
+  const formik = useFormik<LoginType>({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: LoginValidate,
+    onSubmit: (values: LoginType) => {
+      getSuccess(JSON.stringify(values));
+    },
   });
-
-  const dataLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginData({ ...loginData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    LoginValidate.validate(loginData)
-      .then(() => {
-        getSuccess(JSON.stringify(loginData));
-      })
-      .catch((error) => {
-        getError(error.message);
-      });
-  };
 
   return (
     <Container maxWidth="sm">
@@ -45,14 +37,14 @@ export const LoginPage: React.FC<{}> = () => {
         direction="column"
         alignItems="center"
         justifyContent="center"
-        sx={{ minHeight: "100vh" }}
+        sx={{ minHeight: '100vh' }}
       >
         <Grid item>
-          <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
+          <Paper sx={{ padding: '1.2em', borderRadius: '0.5em' }}>
             <Typography sx={{ mt: 1, mb: 1 }} variant="h4">
               Iniciar sesion
             </Typography>
-            <Box component="form" onSubmit={handleSubmit}>
+            <Box component="form" onSubmit={formik.handleSubmit}>
               <TextField
                 name="username"
                 margin="normal"
@@ -60,7 +52,12 @@ export const LoginPage: React.FC<{}> = () => {
                 fullWidth
                 label="Email"
                 sx={{ mt: 2, mb: 1.5 }}
-                onChange={dataLogin}
+                value={formik.values.username}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.username && Boolean(formik.errors.username)
+                }
+                helperText={formik.touched.username && formik.errors.username}
               />
               <TextField
                 name="password"
@@ -69,7 +66,12 @@ export const LoginPage: React.FC<{}> = () => {
                 fullWidth
                 label="Password"
                 sx={{ mt: 1.5, mb: 1.5 }}
-                onChange={dataLogin}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
               />
 
               <Button
