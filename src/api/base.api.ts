1c3d258d55826b7232d5cf62ab9e8317bcb16e37
Codrr <coderrdev@gmail.com>
2022-11-17 17:47:38 -0300

    env var

--- src/api/base.api.ts
+++ src/api/base.api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://rickandmortyapi.com/api/"
+const BASE_URL = process.env.REACT_APP_BASEURL
 
 export const instance = axios.create({
     baseURL: BASE_URL
